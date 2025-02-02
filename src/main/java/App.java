282bd176708e9a51c7d55682913f9ec566baf11c
OlegAnisimov <Ealex72@mail.ru>
2025-02-02 21:31:13 +0500

    git commit Spring212

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -8,5 +8,16 @@ public class App {
         HelloWorld bean =
                 (HelloWorld) applicationContext.getBean("helloworld");
         System.out.println(bean.getMessage());
+        HelloWorld bean1 =
+                (HelloWorld) applicationContext.getBean("helloworld");
+        System.out.println(bean1.getMessage());
+        System.out.println(bean1 == bean);
+
+        Cat bean2 = (Cat) applicationContext.getBean("cat");
+        System.out.println(bean2.getName());
+        Cat bean3 = (Cat) applicationContext.getBean("cat");
+        System.out.println(bean3.getName());
+        System.out.println(bean2 == bean3);
+
     }
 }
